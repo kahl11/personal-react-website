b2cb7ae3aa2afc983a5e683411b15d4b45249f89
Kevin Ahl <kahl11@yahoo.ca>
2021-07-09 13:27:59 -0700

    switching over to new mocks

--- src/app.js
+++ src/app.js
@@ -12,19 +12,19 @@ function pageStateReducer(pageState, action) {
     console.log(action)
     switch (action) {
       case 'down':
-        if(pageState.page == 0) return {page: 1, divs:{div1: "-1200"}, color:"#fce181"};
-        return {page: 1, divs:{div1: "-1500"}, color:"#fce181"};
+        if(pageState.page == 0) return {page: 1, divs:{div1: "-1200"}};
+        return {page: 1, divs:{div1: "-1500"}};
       case 'up':
-        if(pageState.page == 0) return {page: 1, divs:{div1: "0"}, color: green};
-        if(pageState.page == 1) return {page: 1, divs:{div1: "0"}, color: green};
-        return {page: 1, divs:{div1: "0"}, color: green};
+        if(pageState.page == 0) return {page: 1, divs:{div1: "0"}};
+        if(pageState.page == 1) return {page: 1, divs:{div1: "0"}};
+        return {page: 1, divs:{div1: "0"}};
       default:
         throw new Error();
     }
   }
 
 export default function App() {
-    const [pageState, updatePageState] = useReducer(pageStateReducer, {page: 0, divs:{div1: "0"}, color: green });
+    const [pageState, updatePageState] = useReducer(pageStateReducer, {page: 0, divs:{div1: "0"}});
     const scrolledEventHandler = (event) => {
         if(event.deltaY > 0){
             updatePageState("down");
@@ -40,12 +40,8 @@ export default function App() {
     }, [])
 
     return (
-        <div className="container is-max-desktop" style={{
-            backgroundColor: pageState.color
-        }}>
+        <div className="main_container">
             <Home className="div1" pageState={pageState} updatePageState={updatePageState}/>
-            
-
         </div>
     );
 }
--- src/js/components/Hamburger.js
+++
@@ -1,4 +0,0 @@
-import React, { useState, useRef, useEffect, Fragment, useCallback } from "react";
-import ReactDOM from "react-dom";
-import "./style.scss";
-import "./hamburger.scss"
